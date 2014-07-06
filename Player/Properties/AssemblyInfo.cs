c1c2756419c09df42b800271fc8c669de5e3555a
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2014-07-06 15:31:13 +0200

    Fix newlines
    The build script needs all newlines to be crlf and not lf.

--- Player/Properties/AssemblyInfo.cs
+++ Player/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.39.930.9944")]
-[assembly: AssemblyFileVersion("1.39.930.9944")]
+[assembly: AssemblyVersion("1.39.930.9945")]
+[assembly: AssemblyFileVersion("1.39.930.9945")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
